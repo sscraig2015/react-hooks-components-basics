ce63a741eab3b70e221b5f1392b1d5269dbd06dc
Sean <craig.sean92@gmail.com>
2022-01-02 08:21:55 -0700

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -3,3 +3,8 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+function Article() {
+    return (
+        <div>Dear Reader: Bjarne Stroustrup has the perfect lecture oration.</div>
+    )
+}
